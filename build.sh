a5bca940bead4ea10d19c1d9c75dbfc061d071a8
gugray <gabor@berlin.de>
2025-09-10 13:46:42 +0200

    code under src

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-
-./shader_includes.sh lissaj
-
 mkdir -p bin
 
+./shader_includes.sh src/lissaj
+
+cd src
+
 g++ main.cpp fps.cpp geo.cpp horrors.cpp lissaj/lissaj.cpp \
-  -o bin/shanat \
+  -o ../bin/shanat \
   -std=c++11 -I/usr/include/drm \
   -lEGL -lGLESv2 -lgbm -ldrm -lpthread -lm
+
+cd ..
