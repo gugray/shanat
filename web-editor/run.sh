e83c3003ff32df1c6f26f50865ad6c428c059244
gugray <gabor@berlin.de>
2025-10-21 17:05:02 +0200

    web editor / running on laxy

--- web-editor/run.sh
+++ web-editor/run.sh
@@ -4,4 +4,4 @@ BASEDIR=/home/gabor/shader-live
 export SHADER_DIR=$BASEDIR
 export STORAGE_DIR=$BASEDIR/data
 
-nohup node src-server/server.js >$BASEDIR/data/server.log  2>&1 &
+nohup node src-server/run.js >$BASEDIR/data/server.log  2>&1 &
